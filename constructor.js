f776c00513cfe444ef058d6a49ab962c6d800016
Brock200081 <Brconnors27@gmail.com>
2024-06-14 10:00:48 -0400

    enter school works

--- constructor.js
+++ constructor.js
@@ -1,7 +1,5 @@
 
- var pcDodge = false;
- const classData = [["knight",1200,150,1,"ace.png"],["holy_knight",2250,350,1,"ace.png"],["theif",1000,150,5,"ace.png"],["assassin",2007,8000,10,"ace.png"],["criminal",750,200,1,"ace.png"],["cult",100,5,1,"ace.png"],["mage",1000,600,1,"ace.png"],["rogue",8000,3000,1,"ace.png"]];
-
+ const classData = [["knight",1200,150,"ace.png"],["holy_knight",2250,350,"ace.png"],["theif",1000,150,"ace.png"],["assassin",2007,8000,"ace.png"],["criminal",750,200,"ace.png"],["cult",100,5,"ace.png"],["mage",1000,600,"ace.png"],["rogue",8000,3000,"ace.png"]];
 
 class Character {
   constructor(name, charClass) {
@@ -10,17 +8,17 @@ class Character {
     this.health = setHP(charClass);
     this.strength = setDamage(charClass);
     this.picture = setPhoto(charClass);
-	this.stealth = setStealth(charClass);
     this.xp = 0; // XP is always zero for new characters
-	this.backPack = setInventory();
+
   }
   // Return the character description
   // Return the character description
 describe() {
-    return `${this.name} is a ${this.charClass} with ${this.stealth} stealth, ${this.health} health points, ${this
+    return `${this.name} is a ${this.charClass} with ${this.health} health points, ${this
   .strength} as strength and ${this.xp} XP points`;
   }
-  illustrate(){
+
+illustrate(){
     return  "<img src =\""+`${this.picture}`+ "\">"
   }
 }
@@ -48,13 +46,6 @@ function setDamage(charClass) {
 function setPhoto(charClass) {
  for(let myClass = 0; myClass < classData.length; myClass++){
    if(charClass == classData[myClass][0]){
-     return classData[myClass][4];
-   }
-  }
-}
-function setStealth(charClass) {
- for(let myClass = 0; myClass < classData.length; myClass++){
-   if(charClass == classData[myClass][0]){
      return classData[myClass][3];
    }
   }
@@ -62,32 +53,10 @@ function setStealth(charClass) {
 
 function setup(){
   classButtons.style.display = "block";
-  h2.style.display = "none" 
-}
-function setInventory() {
- let myInventory = ["stick"];
-  return myInventory;
+  let play = document.getElementById("play");
+  play.style.display = "none";
 }
 
 function getName(){
   return this.name;
-}
-function getStealth() {
-  return this.stealth;
-}
-
-function getHP() {
-  return this.HP;
-}
-
-function getDamage() {
-  return this.Damage;
-}
-
-function getPhoto() {
-  return this.Photo;
-}
-
-function getInventory() {
-  return this.backPack;
 }
